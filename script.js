84e69610d0c305ca571ed649c5079cbb0f42c13e
Mark <hili.foxiris@gmail.com>
2020-08-29 23:05:45 +0300

    15 lesson

--- script.js
+++ script.js
@@ -10,9 +10,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastSeenMovie = prompt('What of the latest movies that you have seen?', '');
-const scoreOfTheLastMovie = prompt('How do you score it?','');
+for(let i=0;i<2;i++){
+    const lastSeenMovie = prompt('One of the latest movie that you have seen?', '');
+    const scoreOfTheLastMovie = prompt('How do you score it?','');
+    if(scoreOfTheLastMovie.length>1||lastSeenMovie>1||lastSeenMovie!=null||scoreOfTheLastMovie!=null){
+        personalMovieDB.movies[lastSeenMovie]= scoreOfTheLastMovie;
+    }else{
+    i--;
+    }
+}
 
-personalMovieDB.movies[lastSeenMovie]= scoreOfTheLastMovie;
+if(personalMovieDB.count<10){
+    console.log("Too less films");
+}
+else if(personalMovieDB.count>10&&personalMovieDB.count<30){
+    console.log("You are a movielover");
+}
+else if(personalMovieDB.count>=30){
+    console.log("Keep going!");
+}
+else{
+    console.log("Error...");
+}
 
 console.log(personalMovieDB);
